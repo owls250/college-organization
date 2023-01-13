b98494b66687717d6ff348448a1aab78ae9546e9
owls250 <68567021+owls250@users.noreply.github.com>
2023-01-12 20:34:04 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function readingTextChange( ) {
+    document.getElementById("readings").innerHTML = 
+    <div>
+    <p>reading to do</p>
+    <p>Class 1</p>
+    </div>
+    ;
+}
+
+function readingTextBack() {
+    //document.getElementById("readings").innerHTML = "Readings";
+}
